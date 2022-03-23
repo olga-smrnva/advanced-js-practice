96e70866a32685ff80d8f76f3e70ca68e1defa03
Olga Smirnova <smrnva.olga@gmail.com>
2022-03-23 15:43:13 +0200

    Higher-order Func-s

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+//Advanded Functions
+//131
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function(str, fn) {
+    console.log(`Result: ${fn(str)}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+const high5 = function() {
+    console.log('✋🏼');
+};
+document.body.addEventListener('click', high5);
